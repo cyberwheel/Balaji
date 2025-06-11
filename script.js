a1de6b5f56326da1448ffff6c469787e8f5f18e0
cyberwheel <cyberwheel.dev@gmail.com>
2025-06-11 08:59:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+ // Simple appointment form feedback
+document.getElementById('appointment-form')?.addEventListener('submit', function(e) {
+  alert('Thank you! Your appointment request has been sent. We will call you shortly.');
+});
